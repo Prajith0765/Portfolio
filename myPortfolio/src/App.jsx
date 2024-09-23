cf8cbc5691a361b873c81a127d3a5b16a4f6f953
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2024-09-23 23:25:57 +0530

    changes

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Navbar from './components/navBar';
+import Home from './components/Home';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   )
 }
