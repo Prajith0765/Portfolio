b689e95a91cf4314e391b5db31f0ccefab94e201
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2024-10-04 20:58:13 +0530

    changes

--- myPortfolio/src/components/Home.jsx
+++ myPortfolio/src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     from-black via-black to-gray-800">
         <div className="max-w-screen-lg mx-auto flex flex-col
             items-center justify-center h-full px-4 md:flex-row">
-            <div className="flex flex-col justify-center h-full">
+            <div className="flex flex-col justify-center h-full mt-10">
                 <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Full Stack Developer </h2>
 
                 <p className="text-gray-300 py-4 max-w-md">
